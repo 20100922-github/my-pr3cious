677dec55eaadf33a0c4a9ce834e97eef24df8351
20100922-github <20100922-github@schleinkofer.name>
2012-05-24 20:50:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyPr3cious::Application.config.secret_token = '4356bb243f51f342f2b1c71b5afea3a91d05c6c9c12511c31498ed442d9eafe98d2aff4cf5d7442dac76ec070208181673c38afd961dd23b60c60c4949f8fef3'
