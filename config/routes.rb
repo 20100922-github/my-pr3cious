776bf084e96cfe82eefd802c077abec32b307703
20100922-github <20100922-github@schleinkofer.name>
2012-05-24 21:17:03 +0200

    basic schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyPr3cious::Application.routes.draw do
+  resources :elements
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ MyPr3cious::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'elements#index'
 
   # See how all your routes lay out with "rake routes"
 
